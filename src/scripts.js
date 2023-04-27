51e49d1c886cda198c71bc9b6eff63d4c0a8d2e3
Notwacob <wilson.a.jacob@gmail.com>
2023-04-27 06:06:13 -0400

    update

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,15 @@
 var $ = require('jquery');
 
-console.log("It's working");
+$('form').submit(function(event){
+    event.preventDefault();
+    $.ajax({
+        url: '/',
+        type: 'POST',
+        data: {
+            email: 'wilson.a.jacob@gmail.com'
+        },
+        success: function(response) {
+            console.log(response)
+        }
+    }); 
+});
