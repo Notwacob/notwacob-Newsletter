e0db12120a4d23a3b52df2b98e67bc84797196bc
Notwacob <wilson.a.jacob@gmail.com>
2023-04-27 04:14:57 -0400

    update

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static(__dirname + '/public'))
+
 app.listen(3000, function() {
     console.log("Listenin on ander 3000")
 });
 
 app.get('/', function(req, res){
-    res.end("Fuck Yeah")
+    res.end("Fuck Yea")
 });
